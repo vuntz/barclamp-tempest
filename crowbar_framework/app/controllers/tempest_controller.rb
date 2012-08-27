406129ad7dcb995597f836479e456dab626af120
Greg Althaus <galthaus@austin.rr.com>
2012-08-27 09:46:33 -0500

    Start port to CB2.0.  Let's get past install first.  Then on to working.

--- crowbar_framework/app/controllers/tempest_controller.rb
+++ crowbar_framework/app/controllers/tempest_controller.rb
@@ -1,4 +1,4 @@
-# Copyright 2011, Dell 
+#
 # Copyright 2012, Dell
 # 
 # Licensed under the Apache License, Version 2.0 (the "License"); 
@@ -16,17 +16,14 @@
 require "rexml/document"
 
 class TempestController < BarclampController
-  def initialize
-    @service_object = TempestService.new logger
-  end
 
   def raise_not_found
     raise ActionController::RoutingError.new('Not Found')
   end
 
   def dashboard
-    @test_runs = @service_object.get_test_runs
-    @ready_nodes = @service_object.get_ready_nodes
+    @test_runs = operations.get_test_runs
+    @ready_nodes = operations.get_ready_nodes
     @nodes_hash = TempestService.get_all_nodes_hash
     render :template => "barclamp/#{@bc_name}/dashboard.html.haml"
   end
@@ -34,12 +31,12 @@ class TempestController < BarclampController
   def test_runs
     # POST /tempest/test_runs/clear
     if (request.post? or request.put?) and params[:id] == 'clear'
-      @service_object.clear_test_runs
+      operations.clear_test_runs
       flash[:notice] = t "barclamp.#{@bc_name}.dashboard.clear.success"
     # POST /tempest/test_runs
     elsif request.post? or request.put?
       begin
-        test_run = @service_object.run_test params[:node]
+        test_run = operations.run_test params[:node]
         flash[:notice] = t "barclamp.#{@bc_name}.run.success", :node => params[:node]
       rescue TempestService::ServiceError => error
         flash[:notice] = t "barclamp.#{@bc_name}.run.failure", :node => params[:node], :error => error
@@ -50,7 +47,7 @@ class TempestController < BarclampController
     
     # GET /tempest/test_runs/<test-run-id>
     elsif uuid = params[:id] 
-      @test_run = @service_object.get_test_run_by_uuid(uuid) or raise_not_found
+      @test_run = operations.get_test_run_by_uuid(uuid) or raise_not_found
       respond_to do |format|
         format.json { render :json => @test_result } 
         format.html { redirect_to "/#{@bc_name}/results/#{uuid}.html" }
@@ -58,7 +55,7 @@ class TempestController < BarclampController
     
     # GET /tempest/test_runs
     else 
-      @test_runs = @service_object.get_test_runs
+      @test_runs = operations.get_test_runs
       respond_to do |format|
         format.json { render :json => @test_runs }
         format.html { nil } # redirect to dashboard
@@ -79,7 +76,7 @@ class TempestController < BarclampController
   end
 
   def results
-    @test_run = @service_object.get_test_run_by_uuid(params[:id])
+    @test_run = operations.get_test_run_by_uuid(params[:id])
     raise_not_found if not @test_run or @test_run['status'] == 'running'
 
     respond_to do |format|
